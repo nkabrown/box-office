ec2818cbc0babac0c5f03e21d4d4bdd47d3553b2
Nathan Brown <nathan.brown@annalect.com>
2022-12-07 23:10:07 -0500

    Configure dev server to serve .scss files

--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -1,7 +1,16 @@
+import { litScss } from 'rollup-plugin-scss-lit';
+import { fromRollup } from '@web/dev-server-rollup';
+
+const scss = fromRollup(litScss);
+
 export default {
   appIndex: 'lib/index.html',
+  mimeTypes: {
+    '**/*.scss': 'js'
+  },
   nodeResolve: true,
   open: true,
+  plugins: [scss()],
   port: 8004,
   rootDir: 'lib',
   watch: true
